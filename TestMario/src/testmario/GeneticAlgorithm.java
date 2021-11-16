03aa00146db123770614e3d8807c035dee48aaea
mariorenaldyy <92909618+mariorenaldyy@users.noreply.github.com>
2021-11-16 11:09:13 +0700

    .

--- TestMario/src/testmario/GeneticAlgorithm.java
+++ TestMario/src/testmario/GeneticAlgorithm.java
@@ -17,13 +17,59 @@ public class GeneticAlgorithm {
         target.set(40, true);
         return target;
     }
-    private Population evolve(Population population){
+    public static final double MUTATION_RATE = 0.25;
+    public static final int NUMB_OF_ELITE_CHROMOSOMES = 1;
+    public static final int TOURNAMENT_SELECTION_SIZE = 4;
+    
+    public Population evolve(Population population){
         return mutatePopulation(crossoverPopulation(population));
     }
     private Population crossoverPopulation(Population population){
-        return population;
+        Population crossoverPopulation = new Population(population.getChromosomes().length);
+        for(int i = 0; i<NUMB_OF_ELITE_CHROMOSOMES;i++){
+            crossoverPopulation.getChromosomes()[i] = population.getChromosomes()[i];
+        }
+        for(int i = NUMB_OF_ELITE_CHROMOSOMES; i<population.getChromosomes().length;i++){
+            Chromosome chromosome1 = selectTournamentPopulation(population).getChromosomes()[0];
+            Chromosome chromosome2 = selectTournamentPopulation(population).getChromosomes()[0];
+            crossoverPopulation.getChromosomes()[i] = crossoverChromosome(chromosome1, chromosome2);
+        }
+        return crossoverPopulation;
     }
     private Population mutatePopulation(Population population){
-        return population;
+        Population mutatePopulation = new Population(population.getChromosomes().length);
+        for(int i = 0; i<NUMB_OF_ELITE_CHROMOSOMES;i++){
+            mutatePopulation.getChromosomes()[i] = population.getChromosomes()[i];
+        }
+        for(int i = NUMB_OF_ELITE_CHROMOSOMES; i<population.getChromosomes().length;i++){
+            mutatePopulation.getChromosomes()[i] = mutateChromosome(population.getChromosomes()[i]);
+        }
+        return mutatePopulation;
+    }
+    private Chromosome crossoverChromosome(Chromosome chromosome1, Chromosome chromosome2){
+        Chromosome crossoverChromosome = new Chromosome(getTarget().realSize());
+        for(int i = 0; i<chromosome1.getGenes().realSize();i++){
+            if(Math.random() < 0.5) crossoverChromosome.getGenes().set(i, chromosome1.getGenes().get(i));
+            else crossoverChromosome.getGenes().set(i, chromosome2.getGenes().get(i));
+        }
+        return crossoverChromosome;
+    }
+    private Chromosome mutateChromosome(Chromosome chromosome){
+        Chromosome mutateChromosome = new Chromosome(getTarget().realSize());
+        for(int i = 0; i<chromosome.getGenes().realSize();i++){
+            if(Math.random() < MUTATION_RATE){
+                if(Math.random() < 0.5) mutateChromosome.getGenes().set(i, true);
+                else mutateChromosome.getGenes().set(i, false);
+            } else mutateChromosome.getGenes().set(i, chromosome.getGenes().get(i));
+        }
+        return mutateChromosome;
+    }
+    private Population selectTournamentPopulation(Population population){
+        Population tournamentPopulation = new Population(TOURNAMENT_SELECTION_SIZE);
+        for(int i = 0; i<TOURNAMENT_SELECTION_SIZE;i++){
+            tournamentPopulation.getChromosomes()[i] = population.getChromosomes()[(int)(Math.random()*population.getChromosomes().length)];
+        }
+        tournamentPopulation.sortChromosomesByFitness();
+        return tournamentPopulation;
     }
 }
--- TestMario/src/testmario/Main.java
+++ TestMario/src/testmario/Main.java
@@ -6,9 +6,19 @@ import java.util.BitSet;
 public class Main {
     public static void main(String[] args) {
         Population population = new Population(GeneticAlgorithm.POPULATION_SIZE).initializePopulation();
+        GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm();
         System.out.println("-------------------------------------------------");
-        System.out.println("Generation # 0 " + " | Fittest chromosome fitness: " + population.getChromosomes()[0].getFitness());
+        System.out.println("Generation # 0" + " | Fittest chromosome fitness: " + population.getChromosomes()[0].getFitness());
         printPopulation(population, "Target Chromosome: " + toString(GeneticAlgorithm.getTarget()));
+        int generationNumber = 0;
+        while(population.getChromosomes()[0].getFitness() < GeneticAlgorithm.getTarget().realSize()){
+            generationNumber++;
+            System.out.println("\n-------------------------------------------------");
+            population = geneticAlgorithm.evolve(population);
+            population.sortChromosomesByFitness();
+            System.out.println("Generation # " +generationNumber+" | Fittest chromosome fitness: " + population.getChromosomes()[0].getFitness());
+            printPopulation(population, "Target Chromosome: " + toString(GeneticAlgorithm.getTarget()));
+        }
     }
     public static void printPopulation(Population population, String heading){
         System.out.println(heading);
